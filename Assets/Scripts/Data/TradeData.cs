be576fbd906ae05428fc6d142a8b2ac359301b5d
westnowjin <101540598+nonactress@users.noreply.github.com>
2026-05-06 22:52:10 +0900

    feat: TradeData ScriptableObject 기본 구조 구현

---
+++ Assets/Scripts/Data/TradeData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TradeData", menuName = "Game/TradeData")]
+public class TradeData : ScriptableObject
+{
+    public ProductType productType;
+    public int costPrice;
+    public int sellPrice;
+    public int Margin => sellPrice - costPrice;
+}
