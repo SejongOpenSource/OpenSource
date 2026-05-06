dfefeab46cf4cfc966c7cfff040d93c5e0164a5e
westnowjin <101540598+nonactress@users.noreply.github.com>
2026-05-06 22:43:47 +0900

    feat: 재고 배열 및 GetStock() 구현

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -4,4 +4,7 @@ public enum ProductType { Onigiri, Ramen, Drink, Lunchbox, Umbrella }
 
 public class Inventory : MonoBehaviour
 {
+    private int[] _stock = new int[5];
+
+    public int GetStock(ProductType type) => _stock[(int)type];
 }
