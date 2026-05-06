aee726c647615f79e78e58297f114c77164d04e7
JO SUBIN <86547611+theFireFly-Night@users.noreply.github.com>
2026-05-06 22:57:04 +0900

    feat: 상권 업그레이드 시스템 구현 (#15)
    * feat: 초기 상가 시스템 구현
    
    * feat: 상가 CSV 생성
    
    * feat: 상권 업그레이드 시스템 구현

--- Assets/Scripts/Player/StoreManager.cs
+++ Assets/Scripts/Player/StoreManager.cs
@@ -5,7 +5,7 @@ public class StoreManager : MonoBehaviour
     [Header("자산 관리")] 
     public int currentMoney { get; private set; } = 500000; // 현재 보유 금액
     public int currentDebt { get; private set; } = 0;        // 현재 대출금
-    
+    public Commerce currentZone = Commerce.Resident;
 
     // [소비] 보유 금액이 충분하면 차감 후 true 반환
     public bool SpendMoney(int amount)
