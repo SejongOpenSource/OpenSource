b983569505f312e02879e9d578a0568db8d98388
theFireFly-Night <whtnqls0830@naver.com>
2026-05-12 11:29:48 +0900

    feat: 당일 손님의 수 계산 로직 구현(GameData 부재로 하드코딩)

--- Assets/Scripts/Player/CustomerManager.cs
+++ Assets/Scripts/Player/CustomerManager.cs
@@ -31,6 +31,30 @@ public class CustomerManager : MonoBehaviour
 
     public int CalculateWeatherVisitor()
     {
-        return baseVisitor;
+        WeatherSystem weather = GetComponent<WeatherSystem>(); //  => GameData로 변경
+        Weather morningWeather = weather.morningWeather;
+        Weather afternoonweather = weather.afternoonWeather;
+        
+        int currnetVisitor = CalculateDailyVisitors();
+        float morningVisitor = CalculateWeatherVisitorWeight(morningWeather,currnetVisitor / 2f);
+        float afternoonVisitor = CalculateWeatherVisitorWeight(afternoonweather,currnetVisitor / 2f);
+        
+        return (int)(morningVisitor + afternoonVisitor);
+    }
+
+    public float CalculateWeatherVisitorWeight(Weather weather, float visitor)
+    {
+        switch (weather)
+        {
+            case Weather.Rainy:
+                return visitor *= 1.05f;
+            case Weather.Cloudy:
+                return visitor *= 0.95f;
+            case Weather.Heatwave:
+                return visitor *= 1.15f;
+            case Weather.Snowy:
+                return visitor *= 0.85f;
+            default: return visitor;
+        }
     }
 }
