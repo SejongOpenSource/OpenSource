f97a014735572448e50b6394c1d34249ba9e7210
westnowjin <101540598+nonactress@users.noreply.github.com>
2026-05-13 22:34:32 +0900

    fix: add null check to items.Find in ItemDataManager

--- Assets/Scripts/Manager/ItemDataManager.cs
+++ Assets/Scripts/Manager/ItemDataManager.cs
@@ -11,6 +11,6 @@ public class ItemDataManager : MonoBehaviour
         // 간단하게 이름이나 타입 매칭 (ItemData에 ItemType 필드가 있는 것이 좋음)
         // 여기서는 인덱스로 접근하거나 이름을 찾음.
         // ItemData를 확장하여 ItemType을 포함하도록 수정 필요.
-        return items.Find(i => i.itemType == type);
+        return items.Find(i => i != null && i.itemType == type);
     }
 }
