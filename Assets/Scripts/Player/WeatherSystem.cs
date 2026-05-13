dea07fb03d84096cd50027998d2a9317acfec03f
theFireFly-Night <whtnqls0830@naver.com>
2026-05-13 22:31:11 +0900

    feat: WeatherData 클래스 설계 및 WeatherType Enum 분리

--- Assets/Scripts/Player/WeatherSystem.cs
+++ Assets/Scripts/Player/WeatherSystem.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 
-public enum Weather { Sunny, Rainy, Heatwave, Cloudy, Snowy }
-
 public class WeatherSystem : MonoBehaviour
 {
-    public Weather morningWeather;
-    public Weather afternoonWeather;
+    public WeatherType morningWeather;
+    public WeatherType afternoonWeather;
 
     // 하루 날씨 생성
     public void GenerateWeather()
@@ -16,52 +14,52 @@ public class WeatherSystem : MonoBehaviour
     }
     
     // 오전 날씨 생성
-    private Weather GetMorningWeather()
+    private WeatherType GetMorningWeather()
     {
         float roll = Random.Range(0f, 100f);
         
-        if (roll < 5f)  return Weather.Heatwave; // 5%
-        if (roll < 10f) return Weather.Snowy;    // 5%
-        if (roll < 35f) return Weather.Rainy;    // 25%
-        if (roll < 65f) return Weather.Cloudy;   // 30%
-        return Weather.Sunny;                    // 35%
+        if (roll < 5f)  return WeatherType.Heatwave; // 5%
+        if (roll < 10f) return WeatherType.Snowy;    // 5%
+        if (roll < 35f) return WeatherType.Rainy;    // 25%
+        if (roll < 65f) return WeatherType.Cloudy;   // 30%
+        return WeatherType.Sunny;                    // 35%
     } 
     
     // 오후 날씨 생성
-    private Weather GetAfternoonWeather(Weather morning)
+    private WeatherType GetAfternoonWeather(WeatherType morning)
     {
         // 50% 확률로 오전 날씨 유지
         if (Random.value < 0.5f) return morning;
 
         switch (morning)
         {
-            case Weather.Snowy:
-                return GetRandomFrom(Weather.Rainy, Weather.Cloudy, Weather.Sunny);
+            case WeatherType.Snowy:
+                return GetRandomFrom(WeatherType.Rainy, WeatherType.Cloudy, WeatherType.Sunny);
                 
-            case Weather.Heatwave:
-                return GetRandomFrom(Weather.Cloudy, Weather.Rainy, Weather.Sunny);
+            case WeatherType.Heatwave:
+                return GetRandomFrom(WeatherType.Cloudy, WeatherType.Rainy, WeatherType.Sunny);
             
-            case Weather.Cloudy:
-                return GetRandomFrom(Weather.Rainy, Weather.Sunny, Weather.Heatwave, Weather.Snowy);
+            case WeatherType.Cloudy:
+                return GetRandomFrom(WeatherType.Rainy, WeatherType.Sunny, WeatherType.Heatwave, WeatherType.Snowy);
             
-            case Weather.Rainy:
-                return GetRandomFrom(Weather.Sunny, Weather.Snowy, Weather.Cloudy);
+            case WeatherType.Rainy:
+                return GetRandomFrom(WeatherType.Sunny, WeatherType.Snowy, WeatherType.Cloudy);
             
-            case Weather.Sunny:
-                return GetRandomFrom(Weather.Heatwave, Weather.Cloudy, Weather.Rainy, Weather.Snowy);
+            case WeatherType.Sunny:
+                return GetRandomFrom(WeatherType.Heatwave, WeatherType.Cloudy, WeatherType.Rainy, WeatherType.Snowy);
             
             default:
-                return (Weather)Random.Range(0, 5);
+                return (WeatherType)Random.Range(0, 5);
         }
     }
     
     // 가능한 날씨 선택
-    private Weather GetRandomFrom(params Weather[] options)
+    private WeatherType GetRandomFrom(params WeatherType[] options)
     {
         if (options.Length == 0)
         {
             Debug.Log("날씨 오류 발생!!!");
-            return Weather.Sunny;
+            return WeatherType.Sunny;
         }
         return options[Random.Range(0, options.Length)];
     }
