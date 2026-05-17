f02b32229afc607b4d5f3d09690c138cfbd6378d
westnowjin <101540598+nonactress@users.noreply.github.com>
2026-05-17 14:47:31 +0900

    refactor(gamemanager): sales 로직 분리 및 의존성 제거
    - SalesSimulationManager.cs 제거 (별도 이슈에서 관리)
    - Inventory.cs 제거 (InventoryManager 싱글톤으로 통합 예정)
    - GameManager: TotalSales, AddSales(), salesSimulationManager, inventory 참조 제거
    - CheckWin() 제거 (sales 데이터 없이 판단 불가)

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,14 +4,11 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public int TotalSales { get; private set; } = 0;
     public int TargetSales { get; private set; } = 5000000;
 
     [HideInInspector] public StoreManager storeManager;
     [HideInInspector] public Loan loan;
     [HideInInspector] public WeatherSystem weatherSystem;
-    [HideInInspector] public Inventory inventory;
-    [HideInInspector] public SalesSimulationManager salesSimulationManager;
 
     private void Awake()
     {
@@ -20,16 +17,11 @@ public class GameManager : MonoBehaviour
         storeManager = GetComponent<StoreManager>();
         loan = GetComponent<Loan>();
         weatherSystem = GetComponent<WeatherSystem>();
-        inventory = GetComponent<Inventory>();
-        salesSimulationManager = GetComponent<SalesSimulationManager>();
     }
-
-    public void AddSales(int amount) => TotalSales += amount;
 
     // TurnManager가 Result 페이즈 완료 시 호출. 게임 종료면 true 반환.
     public bool OnTurnEnd(int currentTurn, int maxTurns)
     {
-        if (CheckWin()) return true;
         if (CheckLose(currentTurn, maxTurns)) return true;
         return false;
     }
@@ -38,13 +30,6 @@ public class GameManager : MonoBehaviour
     {
         return (TurnManager.Instance.MaxTurns - TurnManager.Instance.CurrentTurn) * 10000
                - remainingStockCost - remainingDebt;
-    }
-
-    private bool CheckWin()
-    {
-        if (TotalSales < TargetSales) return false;
-        Debug.Log("승리!");
-        return true;
     }
 
     private bool CheckLose(int currentTurn, int maxTurns)
